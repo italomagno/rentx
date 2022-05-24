bac1575dc702efae31a67fca7542dfc893e10320
Italo Magno <italomagno10@gmail.com>
2022-05-24 20:35:34 -0300

    adding brand,description,name,rent. to details

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
-import { StatusBar } from 'react-native';
 import { BackButton } from '../../components/BackButton';
 import { ImageSlider } from '../../components/ImageSlider';
 import {
  Container,
  Header,
  CarImages,
+ Content,
+ Details,
+ Description,
+ Brand,
+ Name,
+ Rent,
+ Period,
+ Price,
+ About,
+
 } from './styles'
 
 
@@ -24,6 +33,26 @@ return(
      imageUrl={[ 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXeX5cvC0MtQIRdkRFSjBsnhQTCKLXkAU8wzOVLgUjBqPZNBAGfWesaRwqwDqga4VKc7A&usqp=CAU']}
      />
      </CarImages>
+
+     <Content>
+       <Details>
+         <Description>
+           <Brand>Lamborghini</Brand>
+           <Name>Muracan</Name>
+         </Description>
+         <Rent>
+           <Period>AO DIA</Period>
+           <Price>R$ 580</Price>
+         </Rent>
+
+       </Details>
+       <About>
+       Este é automóvel desportivo. Surgiu do lendário touro de lide indultado na praça Real Maestranza de Sevilla.
+        É um belíssimo carro para quem gosta de acelerar.
+
+       </About>
+
+     </Content>
 
  </Container>
  );
