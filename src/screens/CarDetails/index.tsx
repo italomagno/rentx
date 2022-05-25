c31907e1c1e8231961908ceb26c64e4c54a3c315
Italo Magno <italomagno10@gmail.com>
2022-05-24 21:01:22 -0300

    Creating acessory component and adding  Cardetails

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import { BackButton } from '../../components/BackButton';
 import { ImageSlider } from '../../components/ImageSlider';
+
+import SpeedSvg from '../../assets/speed.svg'
+import AccelerationSvg from '../../assets/acceleration.svg'
+import ForceSvg from '../../assets/force.svg'
+import GasolineSvg from '../../assets/gasoline.svg'
+import ExchangeSvg from '../../assets/exchange.svg'
+import PeopleSvg from '../../assets/people.svg'
+
 import {
  Container,
  Header,
@@ -11,11 +19,14 @@ import {
  Brand,
  Name,
  Rent,
+ Acessories,
  Period,
  Price,
  About,
 
+
 } from './styles'
+import { Acessory } from '../../components/Acessory';
 
 
 
@@ -44,8 +55,17 @@ return(
            <Period>AO DIA</Period>
            <Price>R$ 580</Price>
          </Rent>
-
        </Details>
+       
+       <Acessories>
+           <Acessory name='360km/h' icon={SpeedSvg}/>
+           <Acessory name='3.2s' icon={AccelerationSvg}/>
+           <Acessory name='800 HP' icon={ForceSvg}/>
+           <Acessory name='Gasolina' icon={GasolineSvg}/>
+           <Acessory name='Auto' icon={ExchangeSvg}/>
+           <Acessory name='2 pessoas' icon={PeopleSvg}/>
+         </Acessories>
+         
        <About>
        Este é automóvel desportivo. Surgiu do lendário touro de lide indultado na praça Real Maestranza de Sevilla.
         É um belíssimo carro para quem gosta de acelerar.
